dd577864ee831f381f3d1b2d62a1c2e9a9b3d174
Yasir Kavraş <ykavras@Macbook-Pro.local>
2019-11-18 00:34:38 +0300

    cateogory [GET] created and [GET] test created

--- routes/category.js
+++ routes/category.js
@@ -4,12 +4,21 @@ const router = express.Router();
 // GET MODEL SCHEMA
 const Category = require('../models/Category');
 
-router.post('/', function (req, res, next) {
+router.post('/', (req, res, next) => {
 	const category = new Category(req.body);
 	const promise = category.save();
 	promise.then(data => {
 		res.json(data);
 	}).catch(err => {
+		res.json(err)
+	})
+});
+
+router.get('/', (req, res, next) => {
+	const promise = Category.find({});
+	promise.then(data => {
+		res.json(data);
+	}).catch((err) => {
 		res.json(err)
 	})
 });
---
+++ test/api/category.test.js
@@ -0,0 +1,37 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const should = chai.should();
+const server = require('../../app');
+
+chai.use(chaiHttp);
+
+let token, directorId;
+
+describe('Category Tests', () => {
+	before('Get Token', (done) => {
+		chai.request(server)
+			.post('/authenticate')
+			.send({email: 'ykavras@gmail.com', password: '10203040Aa'})
+			.end((err, res) => {
+				if (err)
+					throw err;
+
+				token = res.body.token;
+				done();
+			});
+	});
+	describe('/GET Categories', () => {
+		it('Get all categories records', (done) => {
+			chai.request(server)
+				.get('/api/category')
+				.set('x-access-token', token)
+				.end((err, res) => {
+					if (err)
+						throw err;
+					res.should.have.status(200);
+					res.body.should.be.a('array');
+					done();
+				});
+		});
+	});
+});
