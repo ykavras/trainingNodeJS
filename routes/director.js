e13b7fd362926d4e7572d93cc3f6945549b89533
Yasir Kavraş <ykavras@Macbook-Pro.local>
2019-11-18 00:12:44 +0300

    director [GET] created

--- routes/director.js
+++ routes/director.js
@@ -15,4 +15,50 @@ router.post('/', (req, res, next) => {
 	})
 });
 
+router.get('/', (req, res, next) => {
+	const promise = Director.aggregate([
+		{
+			$lookup: {
+				from: 'movies',
+				localField: '_id',
+				foreignField: 'directorID',
+				as: 'movies'
+			}
+		},
+		{
+			$unwind: {
+				path: '$movies',
+				preserveNullAndEmptyArrays: true
+			}
+		},
+		{
+			$group: {
+				_id: {
+					_id: '$_id',
+					name: '$name',
+					surname: '$surname',
+					bio: '$bio',
+				},
+				movies: {
+					$push: '$movies'
+				}
+			}
+		},
+		{
+			$project: {
+				_id: '$_id._id',
+				name: '$_id.name',
+				surname: '$_id.surname',
+				movies: '$movies'
+			}
+		}
+	]);
+
+	promise.then((data) => {
+		res.json(data);
+	}).catch((err) => {
+		res.json(err);
+	});
+});
+
 module.exports = router;
